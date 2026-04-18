929ed3caa9083ddf37bd08be78d6dbf9dae828e2
Jamie Wannenburg <admin@jamiewannenburg.com>
2026-04-18 06:58:16 +0200

    fixed element shim

--- python/org/uacalc/element/__init__.py
+++ python/org/uacalc/element/__init__.py
@@ -1,14 +1,15 @@
-import sys
+def _load_from_uacalc_lib() -> None:
+    import uacalc_lib as _uacalc_lib
 
-is_jython = sys.platform.startswith('java')
-
-if not is_jython:
-    import uacalc_lib
-
-    _element = getattr(uacalc_lib, 'element')
-
+    _element = getattr(_uacalc_lib, 'element')
+    g = globals()
     for _name in dir(_element):
         if not _name.startswith('_'):
-            globals()[_name] = getattr(_element, _name)
+            g[_name] = getattr(_element, _name)
+
+
+if __import__('sys').platform.startswith('java'):
+    from org.uacalc.element import *  # noqa: F403
 else:
-    from org.uacalc.element import *
+    _load_from_uacalc_lib()
+    del _load_from_uacalc_lib
